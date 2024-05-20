01adb4addf7d0faa96fe2a0358312fb0b5ee4862
Ba31nga <mynameisnati135@gmail.com>
2024-05-20 15:13:08 +0300

    Final Commit

--- app/src/main/java/com/example/mathmate/Fragments/DisplayForumFragment.java
+++ app/src/main/java/com/example/mathmate/Fragments/DisplayForumFragment.java
@@ -1,5 +1,7 @@
 package com.example.mathmate.Fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +9,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.mathmate.R;
 
 
 public class DisplayForumFragment extends Fragment {
 
+    String forumId;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_display_forum, container, false);
+        View v = inflater.inflate(R.layout.fragment_display_forum, container, false);
+
+        SharedPreferences prefs = getContext().getSharedPreferences("PREPS", Context.MODE_PRIVATE);
+        forumId = prefs.getString("forumid", "none");
+        Toast.makeText(getContext(), "id : " + forumId, Toast.LENGTH_SHORT).show();
+
+
+
+        return v;
     }
 }
