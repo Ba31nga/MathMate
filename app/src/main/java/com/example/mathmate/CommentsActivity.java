46d807bf61ce5011bb65395aa805eab24796b1d0
Ba31nga <mynameisnati135@gmail.com>
2024-05-21 16:08:25 +0300

    Final Commit

--- app/src/main/java/com/example/mathmate/CommentsActivity.java
+++ app/src/main/java/com/example/mathmate/CommentsActivity.java
@@ -3,6 +3,7 @@ package com.example.mathmate;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class CommentsActivity extends AppCompatActivity {
 
     private ImageButton go_back_btn;
+    private String forumId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +31,8 @@ public class CommentsActivity extends AppCompatActivity {
         go_back_btn.setOnClickListener(v -> {
             finish();
         });
+
+        forumId = getIntent().getStringExtra("forumid");
+        Toast.makeText(this, forumId, Toast.LENGTH_SHORT).show();
     }
 }
